6231250a0081df7561f29a091a43e39e6cb58070
sborcic <borcicsandro@gmail.com>
2025-03-18 20:29:06 +0100

    Dodane dodatne funkcionalnosti kod pydantic modela vezane uz ograničenje unosa, opisa potrebnog unosa, odabira između nekoliko opcija unosa, te dodatne provjere u mikroservisima i statusne greške ukoliko su zadovoljeni određeni uvjeti...dodano automatsko stvaranje ID-a korisnika i ažuriranje podataka u profilu korisnika ako postoji korisnik toga imena, prezime, emaila, korisničkog imena i lozinke.

--- korisnici.py
+++ korisnici.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from models import Korisnik, Korisnik_prijava, Korisnik_profil, Recenzija
 from typing import List, Literal
+import re
 
 app = FastAPI()
 
@@ -8,14 +9,47 @@ korisnici_db=[]
 
 korisnici_db_profil=[]
 
-@app.post("/korisnik_registracija", response_model=Korisnik)
+@app.post("/korisnik_registracija",response_model=Korisnik)
 def korisnik_unos(korisnik: Korisnik):
+   
+    for svaki_korisnik in korisnici_db:
+        if (svaki_korisnik.korisnicko_ime == korisnik.korisnicko_ime) or (svaki_korisnik.email == korisnik.email):
+            raise HTTPException(status_code=400, detail="Korisnik s tim korisničkim imenom ili emailom već postoji!")
+
+    specijalni_znakovi = r"[!\"#$%&/()=?*]"
+
+    if not re.search(specijalni_znakovi, korisnik.lozinka):
+        raise HTTPException(status_code=400, detail="Lozinka ne sadrži specijalni znak!")
+    
+    korisnik.korisnik_ID= len(korisnici_db)+1    
+       
     korisnici_db.append(korisnik)
     print(korisnici_db)
     return korisnik
 
 @app.post("/korisnik_profil", response_model=Korisnik_profil)
 def korisnik_unos(profil_korisnika: Korisnik_profil):
+    korisnik=next((kor for kor in korisnici_db if kor.email == profil_korisnika.email), None)
+    
+    if not korisnik:
+        raise HTTPException(status_code=404, detail="Korisnik nije pronađen!")
+    
+    profil_korisnika.ime=korisnik.ime
+    profil_korisnika.prezime=korisnik.prezime
+    profil_korisnika.korisnicko_ime=korisnik.korisnicko_ime
+    profil_korisnika.lozinka=korisnik.lozinka
+    
+    
+    for postojeci_profil in korisnici_db_profil:
+        if postojeci_profil.email == profil_korisnika.email:
+            raise HTTPException(status_code=400, detail="Korisnik s tim emailom već postoji!")
+        
+        if postojeci_profil.korisnicko_ime == profil_korisnika.korisnicko_ime:
+            raise HTTPException(status_code=400, detail="Korisnik s tim korisničkim imenom već postoji!")
+        
+        if postojeci_profil.lozinka == profil_korisnika.lozinka:
+            raise HTTPException(status_code=400, detail="Ova lozinka već postoji već postoji!")
+        
     korisnici_db_profil.append(profil_korisnika)
     print(profil_korisnika)
     return profil_korisnika
--- models.py
+++ models.py
@@ -2,27 +2,32 @@ from pydantic import BaseModel, Field
 from typing import Literal, Optional
 from datetime import date
 
-class Korisnik(BaseModel):
-    ime: str
-    prezime: str
+class Korisnik_ime_prezime_lozinka_email_korisnicko_ime(BaseModel):
+    ime: str = Field(max_length=25)
+    prezime: str  = Field(max_length=25)
     email: str
     korisnicko_ime: str
-    lozinka: str
+    lozinka: str = Field(min_length=8, description="Lozinka mora sadržavati jedan specijalni znak!")
+    
+class Korisnik(Korisnik_ime_prezime_lozinka_email_korisnicko_ime):
+    korisnik_ID: Optional[int] = None
     
 class Korisnik_prijava(BaseModel):
     email: str
-    lozinka: str
-    
-class Korisnik_profil(BaseModel):
-    korisničke_informacije: str
-    ime: str
-    prezime: str
-    spol: Literal["Nije odabran", "Muško", "Žensko"]
-    rođendan: date
+    lozinka: str = Field(min_length=8, description="Lozinka mora sadržavati jedan specijalni znak!")
+        
+class Korisnik_profil(Korisnik_ime_prezime_lozinka_email_korisnicko_ime):
+    korisničke_informacije: str = Field(max_length=50)
+    spol: Literal["Nije odabrano", "Muško", "Žensko"] = "Nije odabrano"
+    rođendan: date = Field(description= "format unosa je godina/mjesec/dan ili YYYY/MM/DD")
     država: str
-    županija: str
-    grad: str
-    
+    županija: Optional [str] = None
+    grad: Optional [str] = None
+    telefon: Optional[str] = None
+    adresa: Optional[str] = None
+    postanski_broj: Optional[str] = None
+    status: Literal["Online", "Offline", "Nedostupan za zamjene"]
+        
 class Kolekcije(BaseModel):
     id: int
     naziv: str
